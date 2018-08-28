0b37486e268751e45941c1aa0b1be2406774b65b
David Corbin <daconex@gmail.com>
2018-08-28 17:39:28 -0400

    Initial Commit

---
+++ GCC-CS-MACAddressCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GCC-CS-MACAddressCapture
+//
+//  Created by David Corbin on 8/28/18.
+//  Copyright © 2018 David Corbin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
